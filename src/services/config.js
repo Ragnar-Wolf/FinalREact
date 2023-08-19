8f7acd936dd62af4901463098afa4c6107d5ba24
Gean Lucas <geanlucapakua@gmail.com>
2023-08-19 13:59:21 -0300

    firebase

--- src/services/config.js
+++ src/services/config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBRVR0rRKIgMlWrdO3NMlLab5jVPBFiO3Y",
-  authDomain: "ecommerce-db6a9.firebaseapp.com",
-  projectId: "ecommerce-db6a9",
-  storageBucket: "ecommerce-db6a9.appspot.com",
-  messagingSenderId: "490529028459",
-  appId: "1:490529028459:web:915359379d07f69320742e"
+  apiKey: "AIzaSyB-dKeyUDa5f_9hmlkKI65Ve6c7yK1pCNY",
+  authDomain: "black--wolf3.firebaseapp.com",
+  projectId: "black--wolf3",
+  storageBucket: "black--wolf3.appspot.com",
+  messagingSenderId: "1055867523838",
+  appId: "1:1055867523838:web:ff1c1b02389a7ca87e4baf"
 };
 
 const app = initializeApp(firebaseConfig);
